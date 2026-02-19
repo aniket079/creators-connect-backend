e71f7556fd691cc02ff3bddd8d3ece20fa44cac6
aniket079 <124426864+aniket079@users.noreply.github.com>
2026-02-20 02:10:26 +0530

    serverchnage

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "https://creators-connect-frontend.vercel.app/login",
+  origin: "https://creators-connect-frontend.vercel.app",
   secure: true,
   credentials: true
 }));
