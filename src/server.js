9dd1b48b74f3d82becf7631473259dd5bee21c0c
aniket079 <124426864+aniket079@users.noreply.github.com>
2026-02-20 01:52:31 +0530

    deployment

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 import express from "express";
-// import "dotenv/config"
+import "dotenv/config"
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import assetRoutes from "./routes/assestRoute.js"
 
 // console.log("EMAIL_USER:", process.env.EMAIL_USER);
 // console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
@@ -20,10 +21,12 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: "http://localhost:5173",
+  secure: true,
   credentials: true
 }));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/assets", assetRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
